158fcec5c8326c362b5455267c3be7e33aaa12bd
feigebaobei <18515195415@163.com>
2019-09-23 20:33:18 +0800

    init

---
+++ src/assets/basic.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="basic"></div>
+</template>
+
+<script>
+import comp from '@/components/common/comp.vue'
+export default {
+  data () {
+    return {
+    }
+  },
+  // watch: {},
+  // filters: {},
+  // computed {},
+  components: {
+  },
+  methods: {
+    // init () {}
+  },
+  created () {},
+  mounted () {
+    // init()
+  }
+}
+</script>
+
+<style scoped="">
+@import '@/assets/stylus/basic.styl'
+  .basic
+</style>
