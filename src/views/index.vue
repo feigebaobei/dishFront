54addceb3b9eaa25c8e3067476a3f2144446fdea
feigebaobei <18515195415@163.com>
2019-10-04 15:50:11 +0800

    避免请求多次是否请求

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,7 +4,7 @@
       <div class="left" @click="goto('consumer')">
         <span>我要订餐</span>
       </div>
-      <div class="right" @click="goto('manage')">
+      <div class="right a b" @click="goto('manage')" ref="manage">
         <span>后台管理</span>
       </div>
     </div>
@@ -31,20 +31,25 @@ export default {
       // 是否登录
       // 是 进入相应页面
       // 否 去登录页面
-      api.isLogin().then(res => {
-        console.log(res)
-        this.$router.push({
-          path: '/' + path
+      if (!this.$refs.manage.classList.value.split(' ').includes('wait')) {
+        this.$refs.manage.classList.add('wait')
+        api.isLogin().then(res => {
+          this.$refs.manage.classList.remove('wait')
+          console.log(res)
+          this.$router.push({
+            path: '/' + path
+          })
+        }).catch(err => {
+          this.$refs.manage.classList.remove('wait')
+          console.log(err)
+          this.$router.push({
+            path: '/login',
+            query: {
+              url: this.opUrl(`/${path}`)
+            }
+          })
         })
-      }).catch(err => {
-        console.log(err)
-        this.$router.push({
-          path: '/login',
-          query: {
-            url: this.opUrl(`/${path}`)
-          }
-        })
-      })
+      }
     },
     opUrl (str) {
       return encodeURIComponent(str)
